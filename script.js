292f23ccde21de874d6d7f17fb12f4c02ac1d1d1
emalgeorge1234@gmail.com <emalgeorge1234@gmail.com>
2021-06-17 21:39:16 +0530

    bday index

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "1" && b=="1")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
